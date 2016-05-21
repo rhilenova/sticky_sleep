9e9b7c288a7ea94a9030e17db11428edd555604f
Rhilenova <rhilenova@gmail.com>
2016-05-20 21:42:21 -0400

    Block present and rendering

--- src/main/java/space/rhilenova/mc/sticky_sleep/StickySleep.java
+++ src/main/java/space/rhilenova/mc/sticky_sleep/StickySleep.java
@@ -1,9 +1,14 @@
 package space.rhilenova.mc.sticky_sleep;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import space.rhilenova.mc.sticky_sleep.block.BlockComfortableWeb;
 
 @Mod(modid = StickySleep.MODID, version = StickySleep.VERSION)
 public class StickySleep
@@ -11,12 +16,23 @@ public class StickySleep
     public static final String MODID = "sticky_sleep";
     public static final String VERSION = "1.0";
 
-    @Instance
-    public static StickySleep instance;
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
+    public static Block web;
+
+    public static final CreativeTabs ss_tab = new CreativeTabs(MODID)
     {
+        @Override
+        public Item getTabIconItem()
+        {
+            ItemStack iStack = new ItemStack(web);
+            return iStack.getItem();
+        }
+    };
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        web = new BlockComfortableWeb();
+        GameRegistry.registerBlock(web, "comfyWeb");
     }
     // MinecraftForge.EVENT_BUS.post(new net.minecraftforge.event.entity.player.PlayerWakeUpEvent(this, p_70999_1_, p_70999_2_, p_70999_3_));
 }
--- src/main/java/space/rhilenova/mc/sticky_sleep/block/BlockComfortableWeb.java
+++ src/main/java/space/rhilenova/mc/sticky_sleep/block/BlockComfortableWeb.java
@@ -4,9 +4,18 @@ import net.minecraft.block.BlockWeb;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
+import space.rhilenova.mc.sticky_sleep.StickySleep;
 
 public class BlockComfortableWeb extends BlockWeb
 {
+    public BlockComfortableWeb()
+    {
+        super();
+        this.setCreativeTab(StickySleep.ss_tab);
+        this.setBlockName(StickySleep.MODID + "_comfyWeb");
+        this.setBlockTextureName("minecraft:web");
+    }
+
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player,
                                     int meta, float sideX, float sideY, float sideZ)
