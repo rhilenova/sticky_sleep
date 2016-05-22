6486df5d968d61837fd0a84a7f63f64f6744beaa
Rhilenova <rhilenova@gmail.com>
2016-05-22 14:40:43 -0400

    Tested and fixed for release

--- src/main/java/space/rhilenova/mc/sticky_sleep/StickySleep.java
+++ src/main/java/space/rhilenova/mc/sticky_sleep/StickySleep.java
@@ -13,7 +13,7 @@ import space.rhilenova.mc.sticky_sleep.block.BlockComfortableWeb;
 @Mod(modid = StickySleep.MODID, version = StickySleep.VERSION)
 public class StickySleep
 {
-    public static final String MODID = "stickySleep";
+    public static final String MODID = "sticky_sleep";
     public static final String VERSION = "1.0";
 
     public static Block web;
